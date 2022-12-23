62bf9ca0ccd1b4f59c140c3034807210410118bd
Olaf Kock <olaf.kock+github@liferay.com>
2022-12-23 13:06:29 +0100

    detect sidecar operation (implementation is untested for all cases)

---
+++ healthcheck-operation/src/main/java/com/liferay/portal/healthcheck/operation/RemoteElasticHealthcheck.java
@@ -0,0 +1,61 @@
+package com.liferay.portal.healthcheck.operation;
+
+import com.liferay.portal.health.api.Healthcheck;
+import com.liferay.portal.health.api.HealthcheckBaseImpl;
+import com.liferay.portal.health.api.HealthcheckItem;
+import com.liferay.portal.kernel.module.configuration.ConfigurationProvider;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+
+import java.util.Collection;
+import java.util.Map;
+
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Modified;
+import org.osgi.service.component.annotations.Reference;
+
+@Component(
+		configurationPid = "com.liferay.portal.search.elasticsearch7.configuration.ElasticsearchConfiguration",
+		service=Healthcheck.class
+		)
+public class RemoteElasticHealthcheck extends HealthcheckBaseImpl {
+
+	private final static String LINK = "https://learn.liferay.com/dxp/latest/en/using-search/installing-and-upgrading-a-search-engine/elasticsearch/connecting-to-elasticsearch.html";
+	private final static String MSG = "healthcheck-elasticsearch-sidecar-not-supported";
+	
+	@Override
+	public Collection<HealthcheckItem> check(ThemeDisplay themeDisplay) {
+		boolean remote = false;
+		boolean configured = operationMode!= null || productionModeEnabled!= null;
+		if(configured) {
+			if(productionModeEnabled != null && productionModeEnabled.equals("true")) {
+				remote = true;
+			} else if(operationMode != null && operationMode.equals("REMOTE")) {
+				remote = true;
+			}
+		}
+        return wrap(create(remote, themeDisplay.getLocale(), LINK, MSG, productionModeEnabled, operationMode));
+	}
+
+	@Override
+	public String getCategory() {
+		return "healthcheck-category-operation";
+	}
+	
+	@Reference
+	protected void setConfigurationProvider(ConfigurationProvider configurationProvider) {
+	    // configuration update will actually be handled in the @Modified event,
+		// which will only be triggered in case we have a @Reference to the 
+		// ConfigurationProvider
+	}
+	
+	@Activate
+	@Modified
+	protected void activate(Map<String, Object> properties) {
+		productionModeEnabled = (String) properties.get("productionModeEnabled");
+		operationMode = (String) properties.get("operationMode");
+	}
+
+	volatile String productionModeEnabled;
+	volatile String operationMode;
+}
