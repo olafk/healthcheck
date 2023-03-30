bf98d9b540eec6148e24bf80e71ec8ca11326980
Olaf Kock <olaf.kock+github@liferay.com>
2023-03-30 11:39:42 +0200

    added license header

--- healthcheck-operation/src/main/java/com/liferay/portal/health/operation/BooleanPropertiesPlausibleValuesHealthcheck.java
+++ healthcheck-operation/src/main/java/com/liferay/portal/health/operation/BooleanPropertiesPlausibleValuesHealthcheck.java
@@ -1,10 +1,23 @@
+/**
+ * Copyright (c) 2022-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
 package com.liferay.portal.health.operation;
 
 import com.liferay.portal.health.api.Healthcheck;
 import com.liferay.portal.health.api.HealthcheckItem;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
-import com.liferay.portal.kernel.theme.ThemeDisplay;
 
 import java.util.Collection;
 import java.util.Locale;
