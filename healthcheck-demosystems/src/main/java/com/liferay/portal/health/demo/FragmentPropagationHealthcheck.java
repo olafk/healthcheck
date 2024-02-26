da740eed0d6caa37f8fd75e308fece7fc5cd74b8
Olaf Kock <olaf.kock+github@liferay.com>
2024-02-26 17:21:23 +0100

    Make sure fragment propagation is on in demosystems.

---
+++ healthcheck-demosystems/src/main/java/com/liferay/portal/health/demo/FragmentPropagationHealthcheck.java
@@ -0,0 +1,47 @@
+package com.liferay.portal.health.demo;
+
+import com.liferay.fragment.configuration.FragmentServiceConfiguration;
+import com.liferay.portal.configuration.metatype.bnd.util.ConfigurableUtil;
+import com.liferay.portal.health.api.Healthcheck;
+import com.liferay.portal.health.api.HealthcheckBaseImpl;
+import com.liferay.portal.health.api.HealthcheckItem;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+
+import java.util.Collection;
+import java.util.Locale;
+import java.util.Map;
+
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Modified;
+
+@Component( 
+		configurationPid = "com.liferay.fragment.configuration.FragmentServiceConfiguration",
+		service = Healthcheck.class
+)
+public class FragmentPropagationHealthcheck extends HealthcheckBaseImpl {
+	private static final String LINK = "/group/control_panel/manage?p_p_id=com_liferay_configuration_admin_web_portlet_InstanceSettingsPortlet&p_p_lifecycle=0&p_p_state=maximized&p_p_mode=view&_com_liferay_configuration_admin_web_portlet_InstanceSettingsPortlet_mvcRenderCommandName=%2Fconfiguration_admin%2Fview_configuration_screen&_com_liferay_configuration_admin_web_portlet_InstanceSettingsPortlet_configurationScreenKey=fragments-service-company";
+	private static final String MSG = "healthcheck-fragment-propagation";
+	
+	private boolean propagate = false;
+
+	@Override
+	public Collection<HealthcheckItem> check(long companyId, Locale locale) {
+		return wrap(create(propagate, locale, LINK, MSG));
+	}
+
+	@Override
+	public String getCategory() {
+		return "healthcheck-category-demosystem";
+	}
+
+	@Activate
+	@Modified
+	protected void activate(Map<String, Object> properties) {
+		FragmentServiceConfiguration config = ConfigurableUtil.createConfigurable(FragmentServiceConfiguration.class, properties);
+		propagate = config.propagateContributedFragmentChanges();
+	}
+	
+	static Log _log = LogFactoryUtil.getLog(FragmentPropagationHealthcheck.class);
+}
